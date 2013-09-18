f7860fbd1a353e3f7cbf64a1cda3c26ef3b5f967
Camilo Sperberg <me@unreal4u.com>
2013-09-18 19:08:13 +0200

    Initial commit, v1.1

---
+++ documentation/simple-verifier.php
@@ -0,0 +1,13 @@
+<?php
+
+include('../rutverifier.class.php');
+
+$rutVerifier = new u4u\rutverifier();
+
+var_dump($rutVerifier->isValidRUT('30.686.957-4'));
+var_dump($rutVerifier->isValidRUT('30686957-4'));
+var_dump($rutVerifier->isValidRUT('306869574'));
+
+var_dump($rutVerifier->isValidRUT('30.686.957-0'));
+
+var_dump($rutVerifier->formatRUT(false));
