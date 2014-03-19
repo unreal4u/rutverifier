96d67479c93c71cb503a1720546e2cc402cb1f5b
Camilo Sperberg <me@unreal4u.com>
2014-03-19 22:14:13 +0100

    Uglify working

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,36 +1,51 @@
-/*global module*/
+/*global module:false*/
 module.exports = function (grunt) {
     'use strict';
 
-    var gruntConfig = {};
-    grunt.loadNpmTasks('grunt-contrib-jshint');
-    gruntConfig.jshint = {
-        options : {
-            bitwise : true,
-            camelcase : true,
-            curly : true,
-            eqeqeq : true,
-            forin : true,
-            immed : true,
-            indent : 4,
-            latedef : true,
-            newcap : true,
-            noarg : true,
-            noempty : true,
-            nonew : true,
-            plusplus : true,
-            quotmark : true,
-            regexp : true,
-            undef : true,
-            unused : false,
-            strict : true,
-            trailing : true,
-            maxparams : 3,
-            maxdepth : 4,
-            maxstatements : 50
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        uglify: {
+            dist: {
+                files: {
+                    'js/<%= pkg.name %>.min.js': ['js/src/<%= pkg.name %>.js']
+                }
+            }
         },
-        all : [ 'Gruntfile.js', 'js/src/*.js' ]
-    };
-    grunt.initConfig(gruntConfig);
+        qunit: {
+            files: ['js/test/*.html']
+        },
+        jshint: {
+            options : {
+                bitwise : true,
+                camelcase : true,
+                curly : true,
+                eqeqeq : true,
+                forin : true,
+                immed : true,
+                indent : 4,
+                latedef : true,
+                newcap : true,
+                noarg : true,
+                noempty : true,
+                nonew : true,
+                plusplus : true,
+                quotmark : true,
+                regexp : true,
+                undef : true,
+                unused : false,
+                strict : true,
+                trailing : true,
+                maxparams : 3,
+                maxdepth : 4,
+                maxstatements : 50
+            },
+            all : [ 'Gruntfile.js', 'js/src/*.js' ]
+        }
+    });
 
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-qunit');
+    grunt.loadNpmTasks('grunt-contrib-jshint');
+
+    grunt.registerTask('default', ['jshint', 'uglify']);
 };
